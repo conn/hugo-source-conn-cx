aa28f2f982f02f708834c7d90d6cd7016324a938
JD Friedrikson <jd@conn.cx>
2018-07-14 19:31:32 -0400

    Update deps, add gulp, refactor layouts
    Updating our submodules:
    `hugo-theme-distinct` v1.0.0 -> v2.0.0
    `logo-conn` v1.0.0 -> v1.1.0
    
    The update to the theme is a complete revision that causes us to
    refactor our CSS and layouts a bit. Also, with the addition of gulp, we
    can follow a `src` -> `static` pattern.
    
    `brand.css` and `css.html` removed because `brand.css` has been migrated
    to `hugo-theme-distinct`. With that gone, there's no extra CSS to source
    on all generated pages.
    
    Favicon has been moved to `logo-conn` so we'll just symlink to it from
    now on. It's nice to know that hugo handles symlinks well.
    
    I want canonical URLs for internal links, so `canonifyURLs` is a natural
    choice.  `.Site.BaseURL` to `absURL` where we can, because I prefer it.
    `canonifyURLs` actually should not be needed when `absURL` is used. The
    documentation is unclear on this, but it seems that `relURL` might
    output absolute links when `canonifyURLs` is set to true. This merits
    more investigation if I ever want to specify relative internal links in
    some places.
    
    We're removing the `categories` taxonomy as I only want to go by tags
    (at least for now). I feel uneasy about organizing my posts in a
    hierarchical manner that comes with categories.

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+#!/usr/bin/env nodejs
+
+'use strict';
+
+const gulp = require('gulp');
+const spawn = require('child_process').spawnSync;
+const through = require('through2');
+const del = require('del');
+
+const cmdPipe = function (cmd, args) {
+  return through.obj(function (file, enc, cb) {
+    let child = spawn(cmd, args, { input: file.contents });
+    file.contents = child.stdout;
+    this.push(file);
+    cb(child.error);
+  });
+}
+
+const minify = function (type) {
+  return cmdPipe('minify', ['--type', type]);
+};
+
+const scss = function() {
+  return through.obj(function (file, enc, cb) {
+    let child = spawn(
+        'sassc',
+
+        [
+          '-m', '-t', 'compressed', file.path,
+          ('static/assets/css/' + file.relative.replace('scss', 'css'))
+        ]
+    );
+
+    cb(child.error);
+  });
+};
+
+gulp.task('clean', function () {
+  del.sync([
+    'static/assets/**',
+    '!static/assets/**/.keep'
+  ]);
+});
+
+gulp.task('css', function () {
+  gulp.src('src/css/**/*.css')
+      .pipe(minify('css'))
+      .pipe(gulp.dest('static/assets/css'));
+});
+
+gulp.task('scss', function () {
+  gulp.src(['src/scss/**/*.scss', '!src/scss/**/_*.scss'])
+      .pipe(scss());
+});
+
+gulp.task('js', function () {
+  gulp.src('src/js/**/*.js')
+      .pipe(minify('js'))
+      .pipe(gulp.dest('static/assets/js'));
+});
+
+gulp.task('fonts', function () {
+  gulp.src([
+    'src/fonts/**/*.otf',
+    'src/fonts/**/*.ttf',
+  ]).pipe(gulp.dest('static/assets/fonts'));
+});
+
+gulp.task('images', function () {
+  gulp.src([
+    'src/images/**/*.jpg',
+    'src/images/**/*.png',
+    'src/images/**/*.svg',
+  ]).pipe(gulp.dest('static/assets/images'));
+});
+
+gulp.task('favicon', function () {
+  gulp.src('src/favicon.ico').pipe(gulp.dest('static'));
+});
+
+gulp.task(
+  'default',
+
+  [
+    'clean',
+    'css',
+    'scss',
+    'js',
+    'fonts',
+    'images',
+    'favicon'
+  ]
+);
